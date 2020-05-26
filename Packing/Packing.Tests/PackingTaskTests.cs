2fef92b09f309105a58ce4ec27d39b470a241973
Blitzkweiz <bezlikiy.pianist@gmail.com>
2020-05-26 17:36:42 +0300

    NextFit complete

---
+++ Packing/Packing.Tests/PackingTaskTests.cs
@@ -0,0 +1,64 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Packing.Tests
+{
+    [TestClass]
+    public class PackingTaskTests
+    {
+        private static double[] weights;
+        public static readonly int n = 1000;
+
+        [ClassInitialize]
+        public static void Initialization(TestContext testContext)
+        {
+            Random random = new Random();
+            weights = Enumerable.Repeat(0, n).Select(i => random.NextDouble()).ToArray();
+        }
+
+        [TestMethod]
+        public void NextFitTest()
+        {
+            var actual = PackingTask.NextFit(weights);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.IsTrue(actual[i].Sum(x => x) < 1);
+                Console.WriteLine(i + " container capacity is " + actual[i].Sum(x => x));
+            }
+        }
+
+        [TestMethod]
+        public void FirstFitTest()
+        {
+            var actual = PackingTask.FirstFit(weights);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.IsTrue(actual[i].Sum(x => x) < 1);
+                Console.WriteLine(i + " container capacity is " + actual[i].Sum(x => x));
+            }
+        }
+
+        [TestMethod]
+        public void OrderedFirstFitTest()
+        {
+            var actual = PackingTask.OrderedFirstFit(weights);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.IsTrue(actual[i].Sum(x => x) < 1);
+                Console.WriteLine(i + " container capacity is " + actual[i].Sum(x => x));
+            }
+        }
+
+        [TestMethod]
+        public void BestFitTest()
+        {
+            var actual = PackingTask.BestFit(weights);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.IsTrue(actual[i].Sum(x => x) < 1);
+                Console.WriteLine(i + " container capacity is " + actual[i].Sum(x => x));
+            }
+        }
+    }
+}
---
+++ Packing/Packing/PackingTask.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Packing
+{
+    public static class PackingTask
+    {
+        public static List<double>[] NextFit(double[] weights, int c = 1)
+        {
+            var currentContainer = 0;
+            List<List<double>> containers = new List<List<double>>();
+            containers.Add(new List<double>());
+            foreach(double weight in weights)
+            {
+                if (containers[currentContainer].Sum(x => x) + weight > c)
+                {
+                    currentContainer += 1;
+                    containers.Add(new List<double>());
+                }
+                containers[currentContainer].Add(weight);
+            }
+            return containers.ToArray();
+        }
+
+        public static List<double>[] FirstFit(double[] weights, int c = 1)
+        {
+            //List<double>[] containers = new List<double>[weights.Length];
+            List<List<double>> containers = new List<List<double>>();
+            containers.Add(new List<double>());
+            var i = 0;
+            var j = 0;
+
+            while(i < weights.Length)
+            {
+                while(j < weights.Length)
+                {
+                    if (containers[j].Sum(x => x) + weights[i] <= c)
+                    {
+                        containers[j].Add(weights[i]);
+                        i++;
+                        j--;
+                        if(i == weights.Length)
+                        {
+                            break;
+                        }
+                    }
+                    j++;
+                }
+            }
+
+            return containers.ToArray();
+        }
+
+        public static List<double>[] OrderedFirstFit(double[] weights, int c = 1)
+        {
+            List<double>[] containers = new List<double>[weights.Length];
+            return containers;
+        }
+
+        public static List<double>[] BestFit(double[] weights, int c = 1)
+        {
+            List<double>[] containers = new List<double>[weights.Length];
+            var i = 0;
+            var j = 0;
+            while(i < weights.Length)
+            {
+                while(j < containers.Length)
+                {
+                    var freeSpaces = new List<double>();
+                    foreach(var container in containers)
+                    {
+                        freeSpaces.Add(c - weights[i] - container.Sum(x => x));
+                    }
+                    for(var k = 0; k < freeSpaces.Count; k++)
+                    {
+                        if(freeSpaces[k] < 0)
+                        {
+                            freeSpaces[k] = c + 0.01;
+                        }
+                    }
+                    containers[freeSpaces.IndexOf(freeSpaces.Min())].Add(weights[i]);
+                    i++;
+                    j--;
+                    if(i == weights.Length)
+                    {
+                        break;
+                    }
+                }
+                i++;
+            }
+            return containers;
+        }
+    }
+}
